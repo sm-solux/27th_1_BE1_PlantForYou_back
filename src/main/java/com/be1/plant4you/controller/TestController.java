6eaab41e82651739c6cfecf86af5346bc6c5b150
jkde7721 <jkde7721@naver.com>
2022-07-07 17:52:44 +0900

    [ADD] 테스트용 도메인, 컨트롤러 추가

---
+++ src/main/java/com/be1/plant4you/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.be1.plant4you.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String hello() {
+        return "hello world!";
+    }
+}
---
+++ src/main/java/com/be1/plant4you/domain/Test.java
@@ -0,0 +1,15 @@
+package com.be1.plant4you.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Test {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+}
