efb7d0e9cc62fcc190eccb461e979f2f228a6d0b
jkde7721 <jkde7721@naver.com>
2022-07-07 15:46:30 +0900

    [SETTING] Init Project

---
+++ src/main/java/com/be1/plant4you/Plant4youApplication.java
@@ -0,0 +1,13 @@
+package com.be1.plant4you;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Plant4youApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Plant4youApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/be1/plant4you/Plant4youApplicationTests.java
@@ -0,0 +1,13 @@
+package com.be1.plant4you;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Plant4youApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
