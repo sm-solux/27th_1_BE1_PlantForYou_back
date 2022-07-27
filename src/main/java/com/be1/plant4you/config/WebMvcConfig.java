d3ebc29f3f06f56d0aeaf439ce01bf4792670c7a
jkde7721 <jkde7721@naver.com>
2022-07-27 17:05:00 +0900

    [SETTING] CORS 설정 추가

--- src/main/java/com/be1/plant4you/config/WebMvcConfig.java
+++ src/main/java/com/be1/plant4you/config/WebMvcConfig.java
@@ -3,6 +3,7 @@ package com.be1.plant4you.config;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import javax.persistence.EntityManager;
@@ -13,5 +14,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Bean
     JPAQueryFactory jpaQueryFactory(EntityManager em) {
         return new JPAQueryFactory(em);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8080")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
