b0b1ed6cfe6bae5b96938408c1345fcb9b0fc961
jkde7721 <jkde7721@naver.com>
2022-07-28 13:21:34 +0900

    [FIX] SPA 새로고침 404에러 해결

---
+++ src/main/java/com/be1/plant4you/controller/CustomErrorController.java
@@ -0,0 +1,20 @@
+package com.be1.plant4you.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    private final String ERROR_PATH = "/error";
+
+    @GetMapping(ERROR_PATH)
+    public String redirectRoot() {
+        return "index.html";
+    }
+
+    public String getErrorPath() {
+        return ERROR_PATH;
+    }
+}
