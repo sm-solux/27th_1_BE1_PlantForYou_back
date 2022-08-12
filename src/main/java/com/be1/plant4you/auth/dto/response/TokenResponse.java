b9d4d127fce636037564496ac1927d2a81f72fa8
jkde7721 <jkde7721@naver.com>
2022-08-12 13:39:36 +0900

    [DOCS] Swagger 문서 최종 정리

--- src/main/java/com/be1/plant4you/auth/dto/response/TokenResponse.java
+++ src/main/java/com/be1/plant4you/auth/dto/response/TokenResponse.java
@@ -1,15 +1,20 @@
 package com.be1.plant4you.auth.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+@Schema(description = "재발급된 토큰 응답 DTO")
 @Getter @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class TokenResponse {
 
+    @Schema(description = "재발급된 엑세스 토큰")
     private String accessToken;
+
+    @Schema(description = "재발급된 리프레시 토큰")
     private String refreshToken;
 }
